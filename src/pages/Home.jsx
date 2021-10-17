462177088fabc2066624c92b24f42127d5b21a65
dheerajjha97 <jhadheeraj97@gmail.com>
2021-10-17 16:24:47 +0530

    1 file modified

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import ControlledCarousel from '../components/ControlledCarousel';
 import Menu  from '../components/Menu';
+import QuickLink from '../components/QuickLinks';
 
 const Home=()=>{
 return(
   <>
   <Menu/>
   <ControlledCarousel/>
+  <QuickLink/>
 </>
 )
 
