e64c631b8238d61a870b08246115d1075ca8a934
dheerajjha97 <jhadheeraj97@gmail.com>
2021-10-17 16:41:35 +0530

    1 file modified

--- src/components/QuickLinks.jsx
+++ src/components/QuickLinks.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
-import {Container} from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
-
-const QuickLink=()=>{
-  return(
+const QuickLink = () => {
+  return (
     <Container fluid>
-  <Row>
-    <Col sm> of 1</Col>
-    <Col sm>sm=true</Col>
-    <Col sm>sm=true</Col>
-    <Col sm>sm=true</Col>
-    <Col sm>sm=true</Col>
-    <Col sm>sm=true</Col>
-    <Col sm>sm=true</Col>
-  </Row>
-</Container>
-  )
-}
+      <Row>
+        <Col>
+        <h2>Quick Links </h2>
+        
+        </Col>
+      </Row>
+
+      <Row>
+        <Col sm> of 1</Col>
+        <Col sm>sm=true</Col>
+        <Col sm>sm=true</Col>
+        <Col sm>sm=true</Col>
+        <Col sm>sm=true</Col>
+        <Col sm>sm=true</Col>
+        <Col sm>sm=true</Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default QuickLink;
