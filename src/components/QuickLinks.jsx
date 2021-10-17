410e9eb328c4ae608afb55dd2df36bd291f77d68
dheerajjha97 <jhadheeraj97@gmail.com>
2021-10-17 16:59:05 +0530

    1 file modified

--- src/components/QuickLinks.jsx
+++ src/components/QuickLinks.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const QuickLink = () => {
+const QuickLink=()=>{
   return (
-    <Container fluid className="mx-auto">
+    <Container fluid className="mx-auto m-5 text-center">
       <Row>
         <Col>
         <h2>Quick Links </h2>
