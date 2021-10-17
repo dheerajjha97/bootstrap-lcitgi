153084ea4d7fac3813872d3f50d7d0fe3152fe42
dheerajjha97 <jhadheeraj97@gmail.com>
2021-10-17 16:24:20 +0530

    1 file added

---
+++ src/components/QuickLinks.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const QuickLink=()=>{
+  return(
+    <Container fluid>
+  <Row>
+    <Col sm> of 1</Col>
+    <Col sm>sm=true</Col>
+    <Col sm>sm=true</Col>
+    <Col sm>sm=true</Col>
+    <Col sm>sm=true</Col>
+    <Col sm>sm=true</Col>
+    <Col sm>sm=true</Col>
+  </Row>
+</Container>
+  )
+}
+
+export default QuickLink;
